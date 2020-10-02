3458d19514d2b0a3bc249b631238a4a815539971
Sandeep Nunna <sandeepnunna90@gmail.com>
2020-10-02 12:49:05 -0400

    use NO_ERRORS_SCHEMA to ignore unidentified elements/directive in template

--- src/app/hero/hero.component.shallow.spec.ts
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -1,3 +1,4 @@
+import { NO_ERRORS_SCHEMA } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { HeroComponent } from "./hero.component";
 
@@ -5,13 +6,18 @@ import { HeroComponent } from "./hero.component";
 // Shallow -> excludes testing child components
 // TestBed -> a special module specifically for testing purposes.
 
+// NO_ERRORS_SCHEMA -> Tells the test to ignore unidentified elements or directives
+// in the template eg. igonres router link directive as we have not imported and set it up
+// in this case.
+
 describe('HeroComponent (shallow tests)', () => {
 
   let fixture: ComponentFixture<HeroComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [HeroComponent]
+      declarations: [HeroComponent],
+      schemas: [NO_ERRORS_SCHEMA]
     });
 
     fixture = TestBed.createComponent(HeroComponent);
