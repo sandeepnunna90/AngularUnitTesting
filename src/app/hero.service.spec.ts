d103f381c7f6c28677d6c188e2bef09af4cee614
Sandeep Nunna <sandeepnunna90@gmail.com>
2020-10-03 11:55:16 -0400

    Create a test for hero service with mocked http

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from "@angular/core/testing";
+import { inject, TestBed } from "@angular/core/testing";
 import { HeroService } from "./hero.service";
 import { MessageService } from "./message.service";
 import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
@@ -10,6 +10,7 @@ describe('Hero Service', () => {
 
   let mockMessageService = jasmine.createSpyObj(['add']);
   let httpTestingController: HttpTestingController;
+  let service: HeroService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -22,6 +23,28 @@ describe('Hero Service', () => {
 
     httpTestingController = TestBed.get(HttpTestingController); // get allows to creates a handle
     let messageService = TestBed.get(MessageService); // get allows it to create a handle for the hero service
-
+    service = TestBed.get(HeroService);
   })
+
+  describe('getHeroes', () => {
+    it('should call get the correct URL', () => {
+      service.getHero(4).subscribe();
+
+      const req = httpTestingController.expectOne('api/heroes/4');
+      req.flush({ id: 4, name: 'HawkEye', strength: 6 });
+    });
+  });
+
+  // Alternative method to create handles
+  describe('getHeroes using inject', () => {
+    it('should call get the correct URL',
+      inject([HeroService, HttpTestingController],
+        (heroService: HeroService, testingController: HttpTestingController) => {
+          heroService.getHero(5).subscribe();
+
+          const req = testingController.expectOne('api/heroes/4');
+          req.flush({ id: 5, name: 'BlackWidow', strength: 7 });
+        }));
+  });
+
 });
